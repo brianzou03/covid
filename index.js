f29cf0113a3f7303e9a06948a67c8ace331d99f4
Brian Zou <brianzou03@gmail.com>
2020-09-26 21:40:35 -0700

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
+
+import './styles.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <div className="main-content">
+            <Main />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App></App>, rootElement) 
